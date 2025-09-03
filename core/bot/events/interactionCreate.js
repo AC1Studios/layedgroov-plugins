65af6f5ad5e0ca2a9539c5463baeaae808f55d98
AC1Studios <71151528+AC1Studios@users.noreply.github.com>
2025-09-02 19:11:48 -0500

    Update interactionCreate.js

--- core/bot/events/interactionCreate.js
+++ core/bot/events/interactionCreate.js
@@ -6,53 +6,41 @@ const db = require("../../db.service");
  * @param {import('discord.js').Interaction} interaction
  */
 module.exports = async (interaction) => {
-    if (!interaction.guild) {
-        return interaction
-            .reply({
-                content: "Command can only be executed in a discord server",
-                flags: MessageFlags.Ephemeral,
-            })
-            .catch(() => {});
-    }
-
     const guild = interaction.guild;
 
     // Slash Commands
     if (interaction.isChatInputCommand()) {
         const cmd = interaction.client.commandManager.findSlashCommand(interaction.commandName);
         if (!cmd) {
-            return interaction
-                .reply({
-                    content: guild.getT("core:HANDLER.CMD_NOT_FOUND"),
-                    flags: MessageFlags.Ephemeral,
-                })
-                .catch(() => {});
+            return interaction.reply({
+                content: guild ? guild.getT("core:HANDLER.CMD_NOT_FOUND") : "Command not found.",
+                flags: MessageFlags.Ephemeral,
+            }).catch(() => { });
         }
 
-        const settings = await db.getSettings(guild);
+        if (guild) {
+            const settings = await db.getSettings(guild);
 
-        // check if the plugin is disabled
-        if (!settings.enabled_plugins.includes(cmd.plugin.name)) {
-            return interaction
-                .reply({
+            // Plugin disabled
+            if (!settings.enabled_plugins.includes(cmd.plugin.name)) {
+                return interaction.reply({
                     content: guild.getT("core:HANDLER.PLUGIN_DISABLED"),
                     flags: MessageFlags.Ephemeral,
-                })
-                .catch(() => {});
-        }
+                }).catch(() => { });
+            }
 
-        // check if the command is disabled
-        if (settings.disabled_slash.includes(cmd.name)) {
-            return interaction
-                .reply({
+            // Command disabled
+            if (settings.disabled_slash.includes(cmd.name)) {
+                return interaction.reply({
                     content: guild.getT("core:HANDLER.CMD_DISABLED"),
                     flags: MessageFlags.Ephemeral,
-                })
-                .catch(() => {});
+                }).catch(() => { });
+            }
         }
 
         await handleSlashCommand(interaction, cmd);
     }
+
 
     // Context Menu
     else if (interaction.isContextMenuCommand()) {
@@ -61,6 +49,6 @@ module.exports = async (interaction) => {
         else
             return interaction
                 .reply({ content: "An error has occurred", flags: MessageFlags.Ephemeral })
-                .catch(() => {});
+                .catch(() => { });
     }
 };
